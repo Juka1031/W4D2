ec793ea5063ec7b15a6e9d465c8fb64ad4b2d12d
David Woolner <davidwoolner@gmail.com>
2021-07-06 17:25:08 -0400

    add piece

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,5 +1,20 @@
 class Piece
-    def initialize
-        
+    def initialize(color, board, pos)#color is a symbol
+        @color = color
+        @board = board
+        @pos = pos 
     end
+
+    def to_s
+        @color.to_s 
+    end
+
+    def empty?
+        #why empty? what is empty? 
+    end
+
+    def moves
+        moves = []
+    end
+
 end
