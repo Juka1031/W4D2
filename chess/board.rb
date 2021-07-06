3ddff79a240e786e80a5d89970b5a1f716cff7d5
julian <juka1031@gmail.com>
2021-07-06 16:54:16 -0400

    phase2?

--- chess/board.rb
+++ chess/board.rb
@@ -2,7 +2,7 @@ require_relative "piece"
 
 class Board
     def initialize
-        @rows = Array.new(8) { Array.new(8, Piece.new) } #parameters))} #Piece.new correct?
+        @rows = Array.new(8) { Array.new(8) {Piece.new} } #parameters))} #Piece.new correct?
     end
 
     def [](pos)
@@ -12,10 +12,18 @@ class Board
 
     def []=(pos, val)
         row, col = pos
-        @rows[row][col] = val #val???
+        @rows[row][col] = val #val is a piece.instance
     end
 
-    def move_piece(start_pos, end_pos)
 
+    def move_piece(start_pos, end_pos)
+        # if (self[start_pos].is_a?(NullPiece)) #that means it is a piece
+        #     raise "no piece there"
+        # end
+        # if #piece cannot mvoe to end_POS (out of bounds)? (or a friendly piece already exist at the end pos)
+        # end
+
+        self[end_pos] = self[start_pos]
+        self[start_pos] = "empty" #empty becomes a null piece
     end
 end
