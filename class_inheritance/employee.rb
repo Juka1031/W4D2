f0aa02588ebb168652c841080eab162bf65e4501
julian <juka1031@gmail.com>
2021-07-06 16:07:57 -0400

    finished inheritance

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -2,7 +2,7 @@ require_relative 'manager'
 class Employee
     attr_reader :name, :title, :salary, :boss
 
-    def initialize(name,title,salary,boss=nil)
+    def initialize(name,title,salary,boss)
         @name = name
         @title = title
         @salary = salary
@@ -20,5 +20,5 @@ end
 
 david = Employee.new("David", "TA", 10000, "Darren")
 shawna = Employee.new("Shawna", "TA", 12000, "Darren")
-darren = Employee.new("Darren", "TA Manager", 78000, "Ned")
-ned = Employee.new("Ned", "Founder", 1000000)
+# darren = Employee.new("Darren", "TA Manager", 78000, "Ned")
+# ned = Employee.new("Ned", "Founder", 1000000)
--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -1,24 +1,40 @@
 require_relative 'employee'
-
+    
 class Manager < Employee
+    attr_accessor :employees
     def initialize(name,title,salary,boss)
         super 
         @employees = []
     end
 
     def bonus(multiplier)
-        sum = @employees.inject do |acc,employee|
+        sum = @employees.inject(0) do |acc,employee|
+            if employee.is_a?(Manager)
+                #do recursion
+                employee.employees.each do |ele1|
+                    acc += ele1.salary
+                end
+            end
             acc += employee.salary
         end
         sum*multiplier
     end
 
     def hire(employee)
+        # if employee.is_a?(Manager)
+        #     flattened = employee.employees.flatten
+        #     flattened.each do |ele|
+        #         @employees << ele
+        #     end
+        #     # employee.employees = []
+        #     @employees << Manager.new(employee.name,employee.title,employee.salary,employee.boss,employee.employees)
+        # else
         @employees << employee
+        # end
     end
     
 end
 
 
-ned = Manager.new("Ned", "Founder", 1000000)
-darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
+# ned = Manager.new("Ned", "Founder", 1000000,nil)
+# darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
