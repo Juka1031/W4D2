a935f8acd51275cd19a720baf5c435faa222ee22
julian <juka1031@gmail.com>
2021-07-07 10:44:32 -0400

    hello

---
+++ chess/module_slideable.rb
@@ -0,0 +1,35 @@
+module Slideable
+    HORIZONTAL_DIRS = 
+    [
+    [0,1],
+    [0,2],
+    [0,3],
+    [0,4],
+    ...
+    [0,7]
+    [1,0]
+    [2,0]
+    ...
+    [7,0]
+    [0,-1]
+    ..
+    [0,-7]
+    ]
+
+    DIAGONAL_DIRS = [
+    ]
+
+    def horizontal_dirs
+    end
+
+    def diagonal_dirs
+    end
+
+    def moves
+        possible_mov = [] <--- 16 arrays
+        #we shovel in all moves within the board bounds given a pieces current positon
+        #lets say if piece is at [0,0]
+    end
+
+    
+end
